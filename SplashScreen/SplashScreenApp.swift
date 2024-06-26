2438eae344b7337b76a41fa334c18f2e2a1d7cf7
Jboy422 <gitonly543@gmail.com>
2024-06-26 15:25:45 +0900

    Initial Commit

---
+++ SplashScreen/SplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreenApp.swift
+//  SplashScreen
+//
+//  Created by Jboy422 on 2024/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
